bad011b6cb2776319785a04dabaa0ff120f60be9
neilmarshall <neil.marshall@dunelm.org.uk>
2020-03-19 20:45:34 +0000

    add schemas

--- book-project/server/server.js
+++ book-project/server/server.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.static(`${__dirname}/../public`));
 
+// DB
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/book_db');
+
+const {Book} = require('./models/books');
+const {Store} = require('./models/stores');
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`started listening on post ${port}`);
