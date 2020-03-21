78863b98112c3d1294ac64050dcc66cf7444936c
neilmarshall <neil.marshall@dunelm.org.uk>
2020-03-21 18:03:54 +0000

    add 'add store' POST route

--- book-project/server/server.js
+++ book-project/server/server.js
@@ -4,14 +4,31 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(`${__dirname}/../public`));
+app.use(bodyParser.json());
 
 // DB
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/book_db');
+mongoose.connect(
+    'mongodb://localhost:27017/book_db',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const {Book} = require('./models/books');
-const {Store} = require('./models/stores');
+const {Book} = require(`${__dirname}/models/books`);
+const {Store} = require(`${__dirname}/models/stores`);
+
+// POST
+app.post('/api/add/store', (req, res) => {
+    const store = new Store({
+        name: req.body.name,
+        address: req.body.address,
+        phone: req.body.phone
+    });
+
+    store.save()
+         .then(doc => res.status(200).send(doc))
+         .catch(err => res.status(400).send(err));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
